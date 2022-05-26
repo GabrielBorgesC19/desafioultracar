50c29db04d973af581c1d2a17c8923aa4944c508
GabrielBorgesC19 <gabriel.borgescarvalho7@gmail.com>
2022-05-26 18:51:41 -0300

    challenge ultracar

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,2 @@
-INSERT INTO cliente(endereco, nome) values ('ultracar', 'fabio');
+INSERT INTO EMPRESA  (id, cnpj , nome )VALUES(default,'12345','ultrasoftware');
+INSERT INTO META (id,meta_faturamento,novos_clientes,empresa_id) VALUES (default,'80000','600',1);
